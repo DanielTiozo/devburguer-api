b2a63c7e99d7267fa1f91cc79dbe6fc1c1015545
DanielTiozo <danieltiozo@hotmail.com>
2024-08-20 20:29:40 -0300

    primeiro commit

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,49 @@
+import * as Yup from 'yup';
+import jwt from 'jsonwebtoken';
+import User from '../models/User';
+import authConfig from '../../config/auth';
+
+class SessionController {
+	async store(req, res) {
+		const schema = Yup.object({
+			email: Yup.string().email().required(),
+			password: Yup.string().min(6).required(),
+		});
+
+		const isValid = await schema.isValid(req.body);
+
+		if (!isValid) {
+			return res.status(401).json({ error: 'Make sure your email or password are correct!' });
+		}
+
+		const { email, password } = req.body;
+
+		const user = await User.findOne({
+			where: {
+				email,
+			},
+		});
+
+		if (!user) {
+			return res.status(401).json({ error: 'Make sure your email or password are correct!' });
+		}
+
+		const isSomePassword = await user.comparePassword(password);
+
+		if (!isSomePassword) {
+			return res.status(401).json({ error: 'Make sure your email or password are correct!' });
+		}
+
+		return res.status(201).json({
+			id: user.id,
+			name: user.name,
+			email,
+			admin: user.admin,
+			token: jwt.sign({ id: user.id }, authConfig.secret, {
+				expiresIn: authConfig.expiresIn,
+			}),
+		});
+	}
+}
+
+export default new SessionController();
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+	secret: '73375e5a080c3d72244de89fae86974c',
+	expiresIn: '5d',
+};
