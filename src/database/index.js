6b0841b2787c5cd3a8c9a6f3e3f604c05622e327
DanielTiozo <danieltiozo@hotmail.com>
2024-10-26 12:51:39 -0300

    incluindo .js nas importações

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,10 @@
 import Sequelize from 'sequelize';
 
-import configDatabase from '../config/database';
+import configDatabase from '../config/database.js';
 
-import User from '../app/models/User';
-import Product from '../app/models/Product';
-import Category from '../app/models/Category';
+import User from '../app/models/User.js';
+import Product from '../app/models/Product.js';
+import Category from '../app/models/Category.js';
 import mongoose from 'mongoose';
 
 const models = [User, Product, Category];
