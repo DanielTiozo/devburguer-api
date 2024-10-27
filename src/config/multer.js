0b4acf4a936424ad63aecf4e7900c95dcf4dd4ca
DanielTiozo <danieltiozo@hotmail.com>
2024-10-26 22:04:02 -0300

    fazendo alterações no multer, correção erro com __dirname

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,23 +1,24 @@
 import multer from 'multer';
 import { v4 } from 'uuid';
 
-// import { extname, resolve } from 'node:path';
+import { extname, resolve } from 'node:path';
 
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
 
 export default {
-storage: multer.diskStorage({
-// 	destination: resolve(__dirname, '..', '..', 'uploads'),
-const storageConfig = {
-	destination: resolve(__dirname, '..', '..', 'uploads'),
-	filename: (req, file, callback) => {
-		callback(null, v4() + extname(file.originalname));
-	},
-	}})
+	storage: multer.diskStorage({
+		// 	destination: resolve(__dirname, '..', '..', 'uploads'),
+		const: (storageConfig = {
+			destination: resolve(_dirname, '..', '..', 'uploads'),
+			filename: (req, file, callback) => {
+				callback(null, v4() + extname(file.originalname));
+			},
+		}),
+	}),
 	// },
 };
 
